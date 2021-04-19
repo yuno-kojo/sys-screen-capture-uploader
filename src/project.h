b56c074cb4e14792ae4ca028d5cb750c6136fdc1
Felipe Musse <fdmusse@gmail.com>
2021-04-19 15:40:05 -0300

    Fix author and version in CMake file

--- src/project.h
+++ src/project.h
@@ -3,9 +3,9 @@
 
 #define APP_TITLE "sys-screen-capture-uploader"
 #define APP_AUTHOR "musse"
-#define APP_VERSION  "0.0.10"
+#define APP_VERSION  "0.0.1"
 #define VERSION_MAJOR "0"
 #define VERSION_MINOR "0"
-#define VERSION_MICRO "10"
+#define VERSION_MICRO "1"
 
 #endif // INCLUDE_GUARD
