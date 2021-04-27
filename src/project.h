9367693755a1c8a2d9383e0ed9f3bbaa200c97c3
Felipe Musse <fdmusse@gmail.com>
2021-04-27 20:43:16 -0300

    Update version to 0.0.2

--- src/project.h
+++ src/project.h
@@ -3,9 +3,9 @@
 
 #define APP_TITLE "sys-screen-capture-uploader"
 #define APP_AUTHOR "musse"
-#define APP_VERSION  "0.0.1"
+#define APP_VERSION  "0.0.2"
 #define VERSION_MAJOR "0"
 #define VERSION_MINOR "0"
-#define VERSION_MICRO "1"
+#define VERSION_MICRO "2"
 
 #endif // INCLUDE_GUARD
